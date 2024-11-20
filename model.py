70ee13d5311687fa2e9bd11958b6ef24d0096f25
SanjayBhargavP <120245828+SanjayBhargavP@users.noreply.github.com>
2024-11-20 12:27:43 -0500

    updated VGG Model

--- model.py
+++ model.py
@@ -13,73 +13,13 @@ class ResNet18(nn.Module):
         return self.model(x)
 
 class VGG16(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super(VGG16, self).__init__()
-        self.model = nn.Sequential(
-            # First Block
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Second Block
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Third Block
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Fourth Block
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Fifth Block
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-
-            # Fully connected layers
-            nn.Flatten(),
-            nn.Linear(in_features=512 * 7 * 7, out_features=4096),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(in_features=4096, out_features=2048),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(in_features=2048, out_features=80)
-        )
+        # Load the VGG16 model with Batch Normalization
+        self.model = models.vgg16_bn(weights=None)  # Load without pre-trained weights
+        # the classifier's final layer to match the desired number of classes
+        num_features = self.model.classifier[6].in_features
+        self.model.classifier[6] = nn.Linear(num_features, num_classes)
 
     def forward(self, x):
         return self.model(x)
